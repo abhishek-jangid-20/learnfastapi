f6f5f5c83bc09e53f6da9eca5a3b874b5042588f
Abhishek Jangid <artislove121@gmail.com>
2025-07-06 15:02:49 +0530

    starting the practice with CRUD operation from now after this commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,6 +9,30 @@ def index():
     return {'data':{'name':'rahul'}}
 
 @app.get('/about')
-def about():
-    return {'data':{'about':'hell'}}
+def about(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published == True:
+        return {'data':f'{limit} published blogs'}
+    else:
+        return {'data':f'{limit} unpublished blogs'}
 
+
+@app.get('/about/unpublished')
+def about():
+    return { 'data': 'All the unpublished blogs' }
+
+@app.get('/about/{id}')
+def about(id:int):
+    return { 'data': id }
+
+@app.get('/about/{id}/comments')
+def comments(id):
+    return { 'data': '1'}
+
+class Model1(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Model1):
+    return {'data': f"Blog is created with title as {request.title}"}
