3ead638ddcde7f5c7318d20df05e600fe010b2ae
Abhishek Jangid <artislove121@gmail.com>
2025-07-11 22:57:54 +0530

    show user function

--- Blog/main.py
+++ Blog/main.py
@@ -68,3 +68,11 @@ def create_user(request : schemas.User, db:Session=Depends(get_db)):
     db.commit()
     db.refresh(user)
     return user
+
+
+@app.post('/blog/{id}',response_model=schemas.ShowUser)
+def show(id, db:Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} is not available")
+    return user
